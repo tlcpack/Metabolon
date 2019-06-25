23a7a7d2cbfb284fc8ae624980cf5bd9c84592da
Taylor Cooke <taylorcooke@gmail.com>
2019-06-25 16:16:04 -0400

    working on parsing the date of rss publication

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import feedparser
 import datetime
+import re
 
 # *Task*: Given a Dictionary keyed by Company and valued by RSS feed url, write a function that determines which companies had no activity for a given number of days.
 # *Hints*:
@@ -9,7 +10,23 @@ import datetime
 # • Reply with questions if necessary, or document assumptions used to make progress.
 
 test_dict = {
-    'Craigslist' : ['https://www.craigslist.org/about/best/all/index.rss'],
+    'Diane Rehm' : ['https://dianerehm.org/rss/npr/dr_podcast.xml'],
 }
 
-print(test_dict)
+_my_date_pattern = re.compile(
+    r'(\d{,2})/(\d{,2})/(\d{4}) (\d{,2}):(\d{2}):(\d{2})')
+
+def myDateHandler(aDateString):
+    """parse a UTC date in MM/DD/YYYY HH:MM:SS format"""
+    month, day, year, hour, minute, second = \
+        _my_date_pattern.search(aDateString).groups()
+    return (int(year), int(month), int(day), \
+        int(hour), int(minute), int(second), 0, 0, 0)
+
+feedparser.registerDateHandler(myDateHandler)
+
+
+rss = feedparser.parse('https://dianerehm.org/rss/npr/dr_podcast.xml')
+print(rss.feed.published)
+
+print(datetime.datetime.now())
