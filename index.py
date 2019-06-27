9cff3cf22242f1ea8b0d845ed6e1d98f1bca9a13
Taylor Cooke <taylorcooke@gmail.com>
2019-06-27 15:47:06 -0400

    removed reg exp import

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import feedparser
 import datetime
-import re
 
 # *Task*: Given a Dictionary keyed by Company and valued by RSS feed url, write a function that determines which companies had no activity for a given number of days.
 # *Hints*:
